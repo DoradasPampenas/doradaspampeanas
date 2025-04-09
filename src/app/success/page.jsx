b7f26e7c2b27671235b33f6844c944de6e8cd072
doradaspampeanas <doradaspampeanas@gmail.com>
2025-04-09 13:42:40 +0000

    tercer commit

--- src/app/success/page.jsx
+++ src/app/success/page.jsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
 import { doc, getDocs, updateDoc, collection } from "firebase/firestore";
-import { db } from "@/firebase";
+import { db } from "../firebase";
 
 export default function SuccessPage() {
   const searchParams = useSearchParams();
